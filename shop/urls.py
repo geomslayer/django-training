15eefd50ce56ef5b97f2173dcc2bde7c5e667042
geomslayer <geomslayer@gmail.com>
2017-04-25 19:00:07 +0300

    Add contacts page, add expand-class to price

--- shop/urls.py
+++ shop/urls.py
@@ -4,5 +4,6 @@ from django.conf import settings
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.main_page, name='main_page')
+    url(r'^$', views.products_page, name='products'),
+    url(r'^contacts$', views.contacts_page, name='contacts'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- shop/views.py
+++ shop/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 from .models import Product
 
 
-def main_page(request):
+def products_page(request):
     products = Product.objects.all()
     return render(request, 'shop/product_list.html', {'products': products})
+
+
+def contacts_page(request):
+    return render(request, 'shop/contacts.html')
